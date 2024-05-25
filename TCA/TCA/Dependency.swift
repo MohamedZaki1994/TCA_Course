df71d678588161c6d81456d69b2ab391da70a674
MohamedZaki1994 <mohamed_zaki888@live.com>
2024-05-25 07:57:38 +0300

    dependency

---
+++ TCA/TCA/Dependency.swift
@@ -0,0 +1,24 @@
+//
+//  Dependency.swift
+//  TCA
+//
+//  Created by Mohamed Zaki on 25/05/2024.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+@Reducer
+struct DependencyReducer {
+	
+}
+
+struct DependencyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DependencyView()
+}
