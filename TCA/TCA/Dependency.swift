14b024f06b2d7bf8bea38af70646854d68c7cb4e
MohamedZaki1994 <mohamed_zaki888@live.com>
2024-05-31 07:41:32 +0300

    add delay and use .run

--- TCA/TCA/Dependency.swift
+++ TCA/TCA/Dependency.swift
@@ -11,14 +11,49 @@ import ComposableArchitecture
 @Reducer
 struct DependencyReducer {
 	
+	@Dependency(\.continuousClock) var clock
+	
+	@ObservableState
+	struct State: Equatable {
+		var title = "Hello"
+	}
+	
+	enum Action {
+		case buttonTapped
+		case buttonResponse(String)
+	}
+	
+	var body: some ReducerOf<Self> {
+		Reduce { state, action in
+			switch action {
+			case .buttonTapped:
+				return .run { send in
+					try await clock.sleep(for: .seconds(2))
+//					state.title = "Hello World"
+					await send(.buttonResponse("Hello World"))
+				}
+			case .buttonResponse(let newTitle):
+				state.title = newTitle
+				return .none
+			}
+		}
+	}
 }
 
 struct DependencyView: View {
+	let store: StoreOf<DependencyReducer>
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		VStack {
+			Text(store.title)
+			Button("click me") {
+				store.send(.buttonTapped)
+			}
+		}
     }
 }
 
 #Preview {
-    DependencyView()
+	DependencyView(store: Store(initialState: DependencyReducer.State(), reducer: {
+		DependencyReducer()
+	}))
 }
